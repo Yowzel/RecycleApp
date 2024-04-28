b6321afa06ae608e1130729a45f2496ad328637c
Gia-Huy N Gonzalez <39535243+Yowzel@users.noreply.github.com>
2024-04-28 01:27:33 -0700

    adding the rest of it

---
+++ styles.js
@@ -0,0 +1,23 @@
+const stylesheet = createStyleSheet(theme => ({
+    root: {
+      width: 393,
+      height: 852,
+      backgroundColor: 'rgba(255, 254, 248, 1)',
+    },
+    welcomeToFRCycle: {
+      color: 'rgba(152, 197, 171, 1)',
+      textAlign: 'center',
+      fontFamily: 'Krub',
+      fontSize: 36,
+      fontStyle: 'normal',
+      fontWeight: '400',
+    },
+    aPlaceWhereYouCanFindLeftOverFoodRecyclablesFromLocalShopsRestaurants: {
+      color: 'rgba(52, 68, 101, 1)',
+      textAlign: 'center',
+      fontFamily: 'Krub',
+      fontSize: 24,
+      fontStyle: 'normal',
+      fontWeight: '400',
+    },
+  }));
